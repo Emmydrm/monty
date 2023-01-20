e5fd054e6d62ffe13cb4790075213cb77b70156f
Emmydrm <emmypee222@gmail.com>
2023-01-20 03:43:06 -0800

    monty.h

---
+++ monty.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+
+/**
+ * main - monty interpreter
+ * @argc: argument count
+ * @argv: argument variables
+ * Return: EXIT_SUCCESS
+ */
+int main(int argc, char **argv)
+{
+	int montyfile, readfile;
+	char *buffer;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* open file */
+	montyfile = open(argv[1], O_RDONLY);
+	if (montyfile == -1)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	/* read file */
+	buffer = malloc(sizeof(char) * BUFFSIZE);
+	if (buffer == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	readfile = read(montyfile, buffer, BUFFSIZE);
+	if (readfile == -1)
+	{
+		fprintf(stderr, "Error: can't read file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	buffer[readfile] = '\0'; /* null teminate */
+	readmonty(buffer);
+
+	free(buffer);
+	return (EXIT_SUCCESS);
+}
---
+++ monty.h
@@ -0,0 +1,71 @@
+#ifndef MONT_Y
+#define MONT_Y
+
+#define BUFFSIZE 4096
+#define _GNU_SOURCE /* strtok_r */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ctype.h>
+
+extern char *argument;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/** opcode functions */
+void pushnode(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void divelem(stack_t **stack, unsigned int line_number);
+void multelem(stack_t **stack, unsigned int line_number);
+void modelem(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+
+void readmonty(char *buffer);
+void callfunction(stack_t **stack, unsigned int line_number, char *arg1);
+stack_t *create_stack(int n);
+void freestack(stack_t **stack);
+int _isdigit(char *string);
+
+#endif /* MONT_Y */
